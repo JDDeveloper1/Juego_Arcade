006a964df55b90cc1a65e85c62040c1ecca0dfd1
David Soto <93728699+JDDeveloper1@users.noreply.github.com>
2022-05-30 14:16:09 +0200

    First Change

---
+++ Proyecto-juego/script.js
@@ -0,0 +1,33 @@
+//let salas = ["Cordenadas",["Norte","sur","Este","Oeste",[`la primer sala es ${1}`,2,3,4,5,6,7,8,9]]]
+
+//console.log(salas[1][4][1])
+
+let salas1 = [
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+
+]
+
+salas1[0][0] = "primer salon"
+salas1[0][1] = "segundo salon"
+salas1[0][2] = "tercer salon"
+salas1[0][3] = "cuarto salon"
+salas1[1][0] = "quinto salon"
+salas1[1][1] = "sexto salon"
+salas1[1][2] = "septimo salon"
+salas1[1][3] = "octavo salon"
+salas1[2][0] = "noveno salon"
+salas1[2][1] = "decimo salon"
+salas1[2][2] = "once salon"
+salas1[2][3] = "doce salon"
+salas1[3][0] = "trece salon"
+salas1[3][1] = "catorce salon"
+salas1[3][2] = "quince salon"
+salas1[3][3] = "dieciseis salon"
+
+console.log(salas1[1][3])
+
+
+
